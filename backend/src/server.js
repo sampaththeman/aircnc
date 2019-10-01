24e2e0edaefca29df3dccc38808b078ca7cac971
x0n4d0 <romagnolimauricio@gmail.com>
2019-10-01 18:33:09 -0300

    added backend/

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const routes = require('./routes')
+
+const PORT_LISTENING = 3333
+const DB_USER = 'xonado'
+const DB_PASSWD = '12345'
+
+const app = express()
+
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWD}@cluster0-si0th.mongodb.net/semana09?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(PORT_LISTENING)
