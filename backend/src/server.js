dc730d27c6206cddc9b83e9e6bea87671a8cca1e
x0n4d0 <romagnolimauricio@gmail.com>
2019-10-02 17:18:44 -0300

    added cors in app.use(cors())

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const routes = require('./routes')
 
@@ -14,6 +15,7 @@ mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWD}@cluster0-si0th.mongodb.n
   useUnifiedTopology: true,
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
