4a5b46620df362577999560d2ad837721bd5f498
x0n4d0 <romagnolimauricio@gmail.com>
2019-10-05 01:45:16 -0300

    added: import connectionUrl

--- src/frontend/src/services/api.js
+++ src/frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+import connectionUrl from '../services/connectionUrl'
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: `${connectionUrl}`,
 })
 
 export default api
