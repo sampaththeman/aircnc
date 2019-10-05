9887c82a436e9c26be455d45ceabf1b83963a4b8
x0n4d0 <romagnolimauricio@gmail.com>
2019-10-05 00:35:02 -0300

    fixed: Storage... store mothod name error xD

--- src/backend/src/controllers/ApprovalController.js
+++ src/backend/src/controllers/ApprovalController.js
@@ -1,7 +1,7 @@
 const Booking = require('../models/Booking')
 
 module.exports = {
-  async StorageEvent(req, res) {
+  async store(req, res) {
     const { booking_id } = req.params
 
     const booking = await Booking.findById(booking_id).populate('spot')
