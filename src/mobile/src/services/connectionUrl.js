ff2681e61807ebb74dc73fd864812773a57b0368
x0n4d0 <romagnolimauricio@gmail.com>
2019-10-05 01:44:45 -0300

    updated: connectionUrl port

--- src/mobile/src/services/connectionUrl.js
+++ src/mobile/src/services/connectionUrl.js
@@ -1,7 +1,8 @@
 
 // set your emulator IP on Genymotion and change 'localhost'
 const EMULATOR_IP = 'localhost'
+const PORT = '3333'
 
-const connectionUrl = `http://${EMULATOR_IP}:3333`
+const connectionUrl = `http://${EMULATOR_IP}:${PORT}`
 
 export default connectionUrl
