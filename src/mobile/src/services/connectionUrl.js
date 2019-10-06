650e2ceccd4e705718a93759b13c16c7012d9c8b
x0n4d0 <romagnolimauricio@gmail.com>
2019-10-05 22:57:30 -0300

    removed: old server config file

--- src/mobile/src/services/connectionUrl.js
+++
@@ -1,8 +0,0 @@
-
-// set your emulator IP on Genymotion and change 'localhost'
-const EMULATOR_IP = 'localhost'
-const PORT = '3333'
-
-const connectionUrl = `http://${EMULATOR_IP}:${PORT}`
-
-export default connectionUrl
