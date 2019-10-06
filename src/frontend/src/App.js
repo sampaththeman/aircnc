24a566af7034a2b2bcc4e009d8e8a48e7f96808f
x0n4d0 <romagnolimauricio@gmail.com>
2019-10-05 23:48:26 -0300

    added: BrowserRouter and Link to dashboard

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-
-import './App.css';
+import { BrowserRouter, Link } from 'react-router-dom'
+import Routes from './routes'
 
 import logo from './assets/logo.svg';
-
-import Routes from './routes'
+import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <img src={logo} alt="AirCnC"/>
+    <BrowserRouter>
+      <div className="container">
+        <Link to="/dashboard">
+          <img src={logo} alt="AirCnC"/>
+        </Link>
 
-      <div className="content">
-        <Routes />
+        <div className="content">
+          <Routes />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
