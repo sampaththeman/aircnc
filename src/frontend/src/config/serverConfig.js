b290296fa6decfabc5fb6a43d23cbcdf33eb444f
x0n4d0 <romagnolimauricio@gmail.com>
2019-10-05 22:58:24 -0300

    added: serverConfig.js file to get URL

---
+++ src/frontend/src/config/serverConfig.js
@@ -0,0 +1,12 @@
+const IP = 'localhost'
+const PORT = '3333'
+
+export default {
+  URL: `http://${IP}:${PORT}`
+}
+const IP = 'localhost'
+const PORT = '3333'
+
+export default {
+  URL: `http://${IP}:${PORT}`
+}
