1869348133aa6b54747a99ad468f0862c32df3ad
x0n4d0 <romagnolimauricio@gmail.com>
2019-10-07 19:58:01 -0300

    update: Reafactoring and create const PATH_UPLOADS and MONGO_DB_URL

--- src/backend/src/server.js
+++ src/backend/src/server.js
@@ -2,21 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
-
 const socketio = require('socket.io')
 const http = require('http')
-
 const routes = require('./routes')
-
-const PORT_LISTENING = 3333
-const DB_USER = 'user'
-const DB_PASSWD = 'passwd'
-
 const app = express()
 const server = http.Server(app)
 const io = socketio(server)
 
-mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWD}@cluster0-si0th.mongodb.net/semana09?retryWrites=true&w=majority`, {
+const PORT_LISTENING = 3333
+const DB_USER = 'user'
+const DB_PASSWD = 'passwd'
+const PATH_UPLOADS = path.resolve(__dirname, '..', 'uploads')
+const MONGO_DB_URL = `mongodb+srv://${DB_USER}:${DB_PASSWD}@cluster0-si0th.mongodb.net/semana09?retryWrites=true&w=majority`
+
+mongoose.connect(MONGO_DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -38,7 +37,7 @@ app.use((req, res, next) => {
 
 app.use(cors())
 app.use(express.json())
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use('/files', express.static(PATH_UPLOADS))
 app.use(routes)
 
 server.listen(PORT_LISTENING)
